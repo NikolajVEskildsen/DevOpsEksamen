e023a1c40af4594aa5425002c4fe39e9a2d8d5f2
NikolajVE <10503092@ucn.dk>
2026-03-10 11:43:10 +0100

    first pipeline

---
+++ frontend/eslint.config.ts
@@ -0,0 +1,27 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import tseslint from 'typescript-eslint'
+import globals from 'globals'
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    languageOptions: {
+      globals: { ...globals.browser },
+    },
+  },
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: { parser: tseslint.parser },
+    },
+    rules: {
+      'vue/max-attributes-per-line': 'off',
+      'vue/singleline-html-element-content-newline': 'off',
+      'vue/html-self-closing': 'off',
+    },
+  },
+  { ignores: ['dist/**', 'node_modules/**'] },
+)
